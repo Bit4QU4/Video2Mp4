f360ac93ff5f8731980e6ec0defb24dd12cf010a
Bit4QU4 <craftclone1@gmail.com>
2023-03-13 19:04:50 -0700

    Change logging method, change filematching

--- auto-convert-vids.py
+++ auto-convert-vids.py
@@ -3,16 +3,16 @@
 import os
 import subprocess
 import sys
-import fnmatch
 import zipfile
-import shutil
 import ctypes
-import datetime
 import filetype
-#Set variables
-directory = input("Directory to Convert: ")
+import logging
+from tqdm import tqdm
 
-#Set main class
+global directory 
+directory = input("Directory to Convert: ")
+logging.basicConfig(format='%(asctime)s %(message)s', level = logging.DEBUG, filename="OutLog - " + directory)
+
 class VidConvert:
     def ffmpeg_convert(video_input,video_output):
         #try to start ffmpeg exe from comandline
@@ -22,30 +22,27 @@ class VidConvert:
         #if the process errors out then print
         except:
             print("ffmpeg failure, double check install")
-    #magic happens here
+            logging.CRITICAL("ffmpeg is unavailable! Double check install")
+
     def directory_iteration(directory):
         #set working directory
         os.chdir(directory)
         #set base count
         count = 0
+        #shows the user that something is happening
         print("Files Converted: " + str(count))
         #iterate through files in directory specified
-        for filename in os.listdir(directory):
+        for filename in tqdm(os.listdir(directory)):
             if os.path.isdir(filename):
-                print('dir found, skiping')
+                logging.DEBUG("directory found, skipping")
             #if statements to catch files that are already mp4
-            elif fnmatch.fnmatch(filename, '*_converted.mp4'):
-                done = (filename + " Already Done")
-                VidConvert.logger(done,directory)
-            elif fnmatch.fnmatch(filename, '*.mp4'):
-                format = (filename + " Already in mp4 format")
-                VidConvert.logger(format,directory)
-            elif fnmatch.fnmatch(filename, 'Convert_*'):
-                zip_there = ("Folder Already Contains an Output Zip, operation done?")
-                VidConvert.logger(zip_there,directory)
+            elif os.path.splitext(filename)[1] == '.mp4':
+                logging.INFO(filename + " Already in mp4 format")
+            elif 'Convert_' in os.path.splitext(filename)[0]:
+                logging.INFO("Folder Already Contains an Output Zip, operation done?")
                 sys.exit()
             elif filetype.guess(filename) == None:
-                print('unable to determine if file is video type')
+                logging.WARNING("Unable to parse filetype" + filename)
             elif "video" in filetype.guess(filename).mime:
                  input = str(os.path.abspath(directory)) + '\\' + str(os.path.basename(filename))
                  output = str(os.path.abspath(directory)) + '\\' + str(os.path.splitext(filename)[0]) + '_converted.mp4'
@@ -56,19 +53,20 @@ class VidConvert:
                  #updates the count set earlier, keep track of how many files have been Converted
                  count = count + 1
                  #logs some output
-                 VidConvert.logger("File Selected-" + input,directory)
-                 VidConvert.logger("Current Count of File-" + str(count),directory)
-                 VidConvert.logger("File Finished Converting",directory)
+                 logging.DEBUG("File Selected-" + input)
+                 logging.DEBUG("Current Count of File-" + str(count))
+                 logging.DEBUG("File Finished Converting")
             else:
-                print('File not video/ unrecognizable')
+                print('File not video/unrecognizable')
+                logging.INFO("File not video -- skipping: "+ filename)
         #makes a message box to inform user that task has been completed
         ctypes.windll.user32.MessageBoxW(0, "Done, Files Converted: " + str(count), "Conversion Complete", 0x40 | 0x0)
     #Cleans up left over files after they are coppied to the zipfile
     def cleanup(directory):
         for filename in os.listdir(directory):
-            if fnmatch.fnmatch(filename, '*_converted.mp4'):
+            if '_converted' in os.path.splitext(filename)[0]:
                 os.remove(filename)
-            elif fnmatch.fnmatch(filename, 'log_*.txt'):
+            elif 'OutLog - ' in os.path.splitext(filename)[0]:
                 os.remove(filename)
             else:
                 print("File Untouched :" + filename)
@@ -77,14 +75,6 @@ class VidConvert:
         zip = zipfile.ZipFile('Convert_of_'+ str(os.path.basename(directory)) +'.zip','a')
         zip.write(input)
         zip.close()
-    #Logs input(x) and places it into a log file for later checking
-    def logger(x,directory):
-        f= open("log_" +str(os.path.basename(directory))+ ".txt","a+")
-        #appends current date and time to file input
-        f.write(str(datetime.datetime.now())+": " + x)
-        #adds new line character for neat-ness
-        f.write("\n")
-        f.close()
 #main loop
 def main(directory):
     VidConvert.directory_iteration(directory)
